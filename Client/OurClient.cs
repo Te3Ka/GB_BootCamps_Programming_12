906581ebbb10414b740987e8907816cc99167318
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-12 12:42:59 +0300

    Сервер отвечает

--- Client/OurClient.cs
+++ Client/OurClient.cs
@@ -7,12 +7,15 @@ namespace Client
 	{
 		TcpClient _client;
 		StreamWriter _sWriter;
+		StreamReader _sReader;
+
 
 		public OurClient()
 		{
 			_client = new TcpClient("127.0.0.1", 5555);
             _sWriter = new StreamWriter(_client.GetStream(), Encoding.UTF8);
-		
+            _sReader = new StreamReader(_client.GetStream(), Encoding.UTF8);
+
             HandleCommunication();
 		}
 
@@ -25,6 +28,8 @@ namespace Client
                 _sWriter.WriteLine(message);
                 _sWriter.Flush();
 
+                string answerServer = _sReader.ReadLine()!;
+                Console.WriteLine($"Сервер: {answerServer}");
             }
 		}
 	}	
--- Server/OurServer.cs
+++ Server/OurServer.cs
@@ -30,11 +30,17 @@ namespace Server
         void HandleClient(TcpClient client)
         {
             StreamReader _sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
-        
+            StreamWriter _sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+            
             while (true)
             {
                 string message = _sReader.ReadLine();
                 Console.WriteLine($"Client: {message}");
+
+                Console.Write("> ");
+                string answer = Console.ReadLine();
+                _sWriter.WriteLine(answer);
+                _sWriter.Flush();
             }
         }
     }
