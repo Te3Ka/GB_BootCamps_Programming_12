9720d5f458c5cb51a8db8f1752dfb99960de3d36
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-12 12:24:22 +0300

    Сервер готов

---
+++ Client/OurClient.cs
@@ -0,0 +1,31 @@
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+	class OurClient
+	{
+		TcpClient _client;
+		StreamWriter _sWriter;
+
+		public OurClient()
+		{
+			_client = new TcpClient("127.0.0.1", 5555);
+            _sWriter = new StreamWriter(_client.GetStream(), Encoding.UTF8);
+		
+            HandleCommunication();
+		}
+
+		void HandleCommunication()
+		{
+			while (true)
+			{
+				Console.Write("> ");
+				string message = Console.ReadLine()!;
+                _sWriter.WriteLine(message);
+                _sWriter.Flush();
+
+            }
+		}
+	}	
+}
